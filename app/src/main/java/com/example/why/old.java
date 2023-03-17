d20c9a4079264be90b35fc2bf56845e7fed06b35
ayanahye <ayanahye2@gmail.com>
2023-03-17 15:51:03 -0700

    Sixth commit

--- app/src/main/java/com/example/why/old.java
+++ app/src/main/java/com/example/why/old.java
@@ -6,8 +6,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class old extends AppCompatActivity {
+
+    private String[] bookTitles = {
+            "The Great Gatsby",
+            "To Kill a Mockingbird",
+            "1984",
+            "Brave New World",
+            "The Catcher in the Rye",
+            "Lord of the Flies",
+            "Animal Farm",
+            "Pride and Prejudice",
+            "Jane Eyre",
+            "Wuthering Heights",
+            "The Picture of Dorian Gray",
+            "Dracula",
+            "Frankenstein",
+            "The Adventures of Tom Sawyer",
+            "The Adventures of Huckleberry Finn",
+            "Alice's Adventures in Wonderland",
+            "Through the Looking-Glass",
+            "The War of the Worlds",
+            "The Time Machine",
+            "The Invisible Man",
+            "The Hitchhiker's Guide to the Galaxy",
+            "The Lord of the Rings",
+            "The Hobbit",
+            "The Silmarillion",
+            "A Song of Ice and Fire",
+            "The Name of the Wind",
+            "The Wise Man's Fear",
+            "The Way of Kings",
+            "Words of Radiance"
+    };
+
+    // Define a method to generate a random book title
+    public String generateBookTitle() {
+        Random random = new Random();
+        int index = random.nextInt(bookTitles.length);
+        return bookTitles[index];
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +62,19 @@ public class old extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(old.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button generateButton = findViewById(R.id.generateButton);
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Generate a random book title
+                String bookTitle = generateBookTitle();
+
+                // Display the book title in the text view
+                TextView bookTitleTextView = findViewById(R.id.bookTitleTextView);
+                bookTitleTextView.setText(bookTitle);
             }
         });
     }
